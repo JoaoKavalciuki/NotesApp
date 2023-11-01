1230c5327a7a277a52922bb56bc25b6d1fba6050
João <joaokavalciuki@hotmail.com>
2023-11-01 16:56:11 -0300

    Refatoração: Método de retorno mudado para coroutines flow

--- app/src/main/java/com/example/notesapp/database/daos/NoteDao.kt
+++ app/src/main/java/com/example/notesapp/database/daos/NoteDao.kt
@@ -1,10 +1,10 @@
 package com.example.notesapp.database.daos
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.notesapp.database.model.Note
+
 
 @Dao
 interface NoteDao {
@@ -12,7 +12,7 @@ interface NoteDao {
     suspend fun insert(note: Note)
 
     @Query("SELECT * FROM tb_notes")
-    suspend fun getAllNotes() : List<Note>
+    fun getAllNotes(): kotlinx.coroutines.flow.Flow<List<Note>>
 
     @Query("DELETE FROM tb_notes WHERE id =:id")
     suspend fun deleteById(id: Long): Unit
