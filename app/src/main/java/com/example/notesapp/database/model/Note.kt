d1f456f09dc8f61c51371b8751fa30fab2cf94df
João <joaokavalciuki@hotmail.com>
2023-11-20 10:06:36 -0300

    Refatoração: Entidade Nota agora é Parcelable

--- app/src/main/java/com/example/notesapp/database/model/Note.kt
+++ app/src/main/java/com/example/notesapp/database/model/Note.kt
@@ -1,15 +1,18 @@
 package com.example.notesapp.database.model
 
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "tb_notes")
+@Parcelize
 data class Note(
-    var title: String,
-    var content: String,
-) {
 
     @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
+    var id: Int,
+    var title: String,
+    var content: String,
+): Parcelable{
 }
